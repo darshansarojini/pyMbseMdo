fb998fbef59a936fac7f70af4ab37acda128c3c4
suspensemax <120133762+suspensemax@users.noreply.github.com>
2023-03-15 09:54:08 -0700

    Add files via upload

---
+++ Running.m
@@ -0,0 +1,11 @@
+% Enter the following to store paths and file names for the example:
+%
+AeroModel = 'Aerodynamic_11';
+StructModel = 'Structural_11';
+testHarness_struct = 'RollReference_Requirement1_3';
+testFile_struct = 'AutopilotTestFile.mldatx';
+reqDoc = 'RollAutopilotRequirements.txt';
+
+
+%Structure file: 
+open_system(Aerodynamic_11)
