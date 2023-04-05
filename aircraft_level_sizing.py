e6dd1eee97b4c4e018e10aa36d75e4ebac660cd5
Darshan Sarojini <85709297+darshansarojini@users.noreply.github.com>
2023-04-05 10:44:29 -0700

    Computation of aircraft parameters and weights

--- aircraft_level_sizing.py
+++ aircraft_level_sizing.py
@@ -7,6 +7,7 @@ import csdl_om
 
 class AircraftLevelSizing(csdl.Model):
     def initialize(self):
+        self.parameters.declare(name='g', default=9.81, types=float)
         return
 
     def define(self):
@@ -17,11 +18,15 @@ class AircraftLevelSizing(csdl.Model):
         sfc_cr = self.create_input(name='sfc_cr', desc='Cruise SFC')
         BPR = self.create_input(name='BPR', desc='By-pass ratio')
 
+        mLbymTO = self.create_input(name='mLbymTO', desc='Mass ratio, landing - take-off')
+        mOEbymTO = self.create_input(name='mOEbymTO', desc='Relative operating empty mass')
+
         # region Constraint Analysis
         self.add(submodel=ConstraintAnalysis(),
                  name='ConstraintAnalysis', promotes=[])
         self.connect('Aeff', 'ConstraintAnalysis.Aeff')
         self.connect('Phi25', 'ConstraintAnalysis.Phi25')
+        self.connect('mLbymTO', 'ConstraintAnalysis.mLbymTO')
         # endregion
 
         # Mission Analysis
@@ -31,13 +36,25 @@ class AircraftLevelSizing(csdl.Model):
         self.connect('R', 'MissionAnalysis.R')
         self.connect('sfc_cr', 'MissionAnalysis.sfc_cr')
         self.connect('BPR', 'MissionAnalysis.BPR')
+        self.connect('mLbymTO', 'MissionAnalysis.mLbymTO')
+        self.connect('mOEbymTO', 'MissionAnalysis.mOEbymTO')
 
         self.connect('ConstraintAnalysis.TbyW', 'MissionAnalysis.TbyW')
         # endregion
 
-        # # Aircraft parameters
-        # Sw = m_mto / WbyS
-        # Tto = m_mto * self.parameters['g'] * TbyW
+        # region Aircraft parameters
+        WbyS = self.declare_variable(name='WbyS')
+        self.connect('ConstraintAnalysis.WbyS', 'WbyS')
+        TbyW = self.declare_variable(name='TbyW')
+        self.connect('ConstraintAnalysis.TbyW', 'TbyW')
+        m_mto = self.declare_variable(name='m_mto')
+        self.connect('MissionAnalysis.m_mto', 'm_mto')
+
+        Sw = m_mto / WbyS
+        self.register_output(name='Sw', var=Sw)
+        Tto = m_mto * self.parameters['g'] * TbyW
+        self.register_output(name='Tto', var=Tto)
+        # endregion
         return
 
 
@@ -167,6 +184,8 @@ class ConstraintAnalysis(csdl.Model):
         Aeff = self.create_input(name='Aeff', desc='Aspect ratio')
         Phi25 = self.create_input(name='Phi25', units='deg', desc='Sweep angle, at 25% of chord')
 
+        mLbymTO = self.create_input(name='mLbymTO', desc='Mass ratio, landing - take-off')
+
         # region Point performance
         # Landing
         self.add(submodel=Landing(), name='Landing', promotes=[])
@@ -175,6 +194,7 @@ class ConstraintAnalysis(csdl.Model):
         self.add(submodel=Takeoff(), name='Takeoff', promotes=[])
         self.connect('Phi25', 'Takeoff.Phi25')
         self.connect('Landing.mLbySW', 'Takeoff.mLbySW')
+        self.connect('mLbymTO', 'Takeoff.mLbymTO')
         # Glide ratio
         self.add(submodel=GlideRatio(), name='GlideRatio', promotes=[])
         self.connect('Aeff', 'GlideRatio.Aeff')
@@ -192,14 +212,9 @@ class ConstraintAnalysis(csdl.Model):
         TbyW_cr = self.declare_variable(name='TbyW_cr', val=0.30750)
 
         WbyS_takeoff = self.declare_variable(name='WbyS_takeoff')
-        self.print_var(var=WbyS_takeoff)
         self.connect('Takeoff.mTObySW', 'WbyS_takeoff')
         WbyS_landing = self.declare_variable(name='WbyS_landing')
-        self.print_var(var=WbyS_landing)
         self.connect('Landing.mLbySW', 'WbyS_landing')
-
-        self.print_var(var=TbyW_takeoff)
-        self.print_var(var=TbyW_glide)
 
         scaling_parameter_TbyW = 1e3
         TbyW = csdl.max(TbyW_takeoff*scaling_parameter_TbyW,
@@ -207,7 +222,6 @@ class ConstraintAnalysis(csdl.Model):
                         TbyW_ma*scaling_parameter_TbyW,
                         TbyW_cr*scaling_parameter_TbyW,
                         rho=20.)
-        self.print_var(var=TbyW)
         self.register_output(name='TbyW', var=TbyW/scaling_parameter_TbyW)
 
         WbyS = csdl.max(WbyS_takeoff, WbyS_landing)
@@ -234,6 +248,8 @@ class MissionAnalysis(csdl.Model):
         s_alt = self.declare_variable(name='s_alt', desc='Distance to alternate')  # nautical miles
         s_res = self.declare_variable(name='s_res', desc='Reserve flight distance')  # m
         sfc_cr = self.declare_variable(name='sfc_cr', desc='Cruise specific fuel consumption')  # kg/N/s
+        mLbymTO = self.declare_variable(name='mLbymTO', desc='Mass ratio, landing - take-off')
+        mOEbymTO = self.declare_variable(name='mOEbymTO', desc='Relative operating empty mass')
 
         # Glide ratio in cruise
         CLbyCLmd = 1 / VbyVmd**2
@@ -281,18 +297,60 @@ class MissionAnalysis(csdl.Model):
         Mff_standard = Mff_to * Mff_clb * Mff_cr * Mff_des * Mff_l
         Mff_reserve = Mff_clb * Mff_res * Mff_loiter * Mff_des
         Mff_total = Mff_standard * Mff_reserve
-        mFbymMTO = 1 - Mff_total
+        mFbymMTO = 1 - Mff_total  # Mission fuel fraction
 
         # Masses
-        mOEbymMTO = 0.56  # relative operating empty mass
         m_pax = 93  # kg
         n_pax = 180
         m_cargo = 2516  # kg
         m_pl = m_pax * n_pax + m_cargo
-        m_mto = m_pl / (1-mFbymMTO-mOEbymMTO)
+        m_mto = m_pl / (1-mFbymMTO-mOEbymTO)
         self.register_output(name='m_mto', var=m_mto)
+
+        # Mission fuel, standard flight
         m_f = m_mto * mFbymMTO
         self.register_output(name='m_f', var=m_f)
+
+        # Maximum landing mass, based on ratio
+        m_ml = m_mto * mLbymTO
+        self.register_output(name='m_ml', var=m_ml)
+
+        # Operating empty mass
+        m_oe = m_mto * mOEbymTO
+        self.register_output(name='m_oe', var=m_oe)
+
+        # Total fuel mass needed
+        m_f_erf = m_mto * (1-Mff_taxi*Mff_total)
+        self.register_output(name='m_f_erf', var=m_f_erf)
+
+        # Maximum zero-fuel mass
+        m_mzf = m_oe + m_pl
+        self.register_output(name='m_mzf', var=m_mzf)
+
+        # Fuel mass, all reserves
+        m_f_res = m_mto * (1-Mff_reserve)
+
+        # Maximum landing mass, computed by adding masses
+        m_ml_comp = m_mzf + m_f_res
+
+        # Constraint on maximum landing mass
+        # m_ml > m_mzf + m_f_res
+        constr_m_ml = m_ml - m_ml_comp
+        self.register_output(name='Constraint_m_ml', var=constr_m_ml)
+        self.add_constraint(name='Constraint_m_ml', lower=0.)
+        return
+
+
+class FuelTankVolume(csdl.Model):
+    def initialize(self):
+        self.parameters.declare(name='FuelDensity', default=800., types=float)
+        return
+
+    def define(self):
+        m_f_erf = self.declare_variable(name='m_f_erf', desc='Total fuel mass needed (kg)')
+        rho_f = self.parameters['FuelDensity']
+        V_f_erf = m_f_erf/rho_f
+
         return
 
 
@@ -306,11 +364,13 @@ if __name__ == '__main__':
     sim['sfc_cr'] = 1.65E-05  # kg/N/s
     sim['BPR'] = 6.
 
+    sim['mLbymTO'] = 0.878
+    sim['mOEbymTO'] = 0.56
+
     sim['ConstraintAnalysis.Landing.slfl'] = 1448.
     sim['ConstraintAnalysis.Landing.dTl'] = 0.
     sim['ConstraintAnalysis.Landing.CLmax_L_unswept'] = 3.76
 
-    sim['ConstraintAnalysis.Takeoff.mLbymTO'] = 0.878
     sim['ConstraintAnalysis.Takeoff.stofl'] = 1768.
     sim['ConstraintAnalysis.Takeoff.dTto'] = 0.
     sim['ConstraintAnalysis.Takeoff.CLmax_TO_unswept'] = 2.85
@@ -330,6 +390,14 @@ if __name__ == '__main__':
     print('Wing loading at max. takeoff mass: ', sim['ConstraintAnalysis.Takeoff.mTObySW'])
     print('Thrust-to-weight ratio takeoff: ', sim['ConstraintAnalysis.Takeoff.TtobyWto'])
     print('Thrust-to-weight ratio glide: ', sim['ConstraintAnalysis.GlideRatio.TtobyWto'])
+    print('------------------\n------------------')
     print('Sizing thrust-to-weight ratio: ', sim['ConstraintAnalysis.TbyW'])
     print('Sizing wing loading: ', sim['ConstraintAnalysis.WbyS'])
-    print('Aircraft mass (kg) ', sim['MissionAnalysis.m_mto'])
+    print('------------------\n------------------')
+    print('Maximum takeoff mass (kg) ', sim['m_mto'])
+    print('Maximum landing mass (kg) ', sim['MissionAnalysis.m_ml'])
+    print('Mission fuel mass (kg) ', sim['MissionAnalysis.m_f'])
+    print('Total fuel mass (kg) ', sim['MissionAnalysis.m_f_erf'])
+    print('Operating empty mass (kg) ', sim['MissionAnalysis.m_oe'])
+    print('Wing area (m^2) ', sim['Sw'])
+    print('Takeoff thrust, all engines (N) ', sim['Tto'])
