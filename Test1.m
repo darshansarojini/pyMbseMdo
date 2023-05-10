8064f8b997cadf204c6834076a4160caa6988247
suspensemax <120133762+suspensemax@users.noreply.github.com>
2023-05-10 13:20:20 -0700

    Delete Test1.m

--- Test1.m
+++
@@ -1,7 +0,0 @@
-clc; 
-clear;
-v_input=input('Enter value of V ');
-e_input=input('Enter the value of e ');
-E=input('Enter the Value of Youngs Modulus ');
-I=input('Enter the Moment of Inertia ');
-L=input('Enter the Length of the Wing ');
